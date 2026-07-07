9bb4f18bb8b11684dc9d6f49d52deb1b3351e91e
kevinperencin95 <kevin.perencin95@gmail.com>
2026-07-07 13:41:39 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 //  Aggiornamento: automatico al riavvio dell'app
 // ================================================================
 
-const CACHE_VERSION = 'cam-registro-v33';
+const CACHE_VERSION = 'cam-registro-v35';
 const CACHE_FILES   = [
   '/AUTISTISDAPG/',
   '/AUTISTISDAPG/index.html',
